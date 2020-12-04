45d5e660738255665f1ac2c80db2ec24d6a9c743
vincentLuk <luk96515@gmail.com>
2020-12-04 11:35:28 +0800

    heroku

--- db.ts
+++ db.ts
@@ -2,12 +2,20 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// export let client = new pg.Client({
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_ADMIN_USERNAME,
+//     password: process.env.DB_ADMIN_PASSWORD
+// });
+
+
 export let client = new pg.Client({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_ADMIN_USERNAME,
-    password: process.env.DB_ADMIN_PASSWORD
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
+
+
 
 client
   .connect()
